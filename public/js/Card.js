69169aa712b0d989bbfa3fafda6cbe05ac60a885
Amnon Attali <Amnon Attali>
2016-11-12 15:34:13 -0500

    bla

--- public/js/Card.js
+++ public/js/Card.js
@@ -2,17 +2,14 @@
 
 
 var Card = function(suit, value, img){
-    
+
     var that = Object.create(Card.prototype);
-    //var owner = 
-    
-    
+
     that.getSuit  = function(){ return suit;  }
     that.getValue = function(){ return value; }
     that.getImg   = function(){ return img;   }
-    
-    
-    /*that.move = function(newOwner){
-        owner = newOwner;
-    }*/
+    that.getIndex = function() {
+      return (suit * 13 + value);
+    }
+
 }
