48fa394fd3ca88f8ba8e99b3cc0260cdc857466d
Amnon Attali <Amnon Attali>
2016-11-12 15:18:06 -0500

    Changes

--- Card.js
+++ Card.js
@@ -4,9 +4,15 @@
 var Card = function(suit, value, img){
     
     var that = Object.create(Card.prototype);
+    //var owner = 
+    
     
     that.getSuit  = function(){ return suit;  }
     that.getValue = function(){ return value; }
     that.getImg   = function(){ return img;   }
     
+    
+    /*that.move = function(newOwner){
+        owner = newOwner;
+    }*/
 }
---
+++ Location.js
@@ -0,0 +1,4 @@
+
+var Location = function(){
+    
+}
